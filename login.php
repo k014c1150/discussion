85a64f4d9739b3888438fb31b104c27b0a714fdf
tada <k014c1148@it-neec.jp>
2016-09-12 13:41:11 +0900

    login

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="main.css">
+		<style></style>
+		<title>ログイン画面</title>
+	</head>
+	<body>
+	<div class=room>
+		<h1>Spechen</h1>
+		<form action="lobby.html" method="post">
+		<p>
+		名前
+		<input type="text"id="name" name="name" size="30" maxlength="10" value="">
+		<button type="submit" name="example" value="ボタン">login</button
+		</p>
+	</form>
+	</div>
+	</body>
+</html>
