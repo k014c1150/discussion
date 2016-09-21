c76c1d10bc363bd213323e6f94846e61f5b668c2
root <k014c1287@it-neec.jp>
2016-09-21 11:01:41 +0900

    作業の保存

--- lobby.php
+++ lobby.php
@@ -3,7 +3,9 @@ $room_name = array();
 //$room_time;
 //$start_time;
 $room_type = array();  // 0 = １５分　1=３０分 ２ = 一時間
-$room_state = array(); // -1 = ディスカッション中 　0 = 待機　1以上　= 現在の待機人数
+$room_state = array(); // true = ディスカッション中 　false = 待機
+$room_member = array();
+$room_id = array();
 $name = $_POST["name"];
 $ROOM_TIME = array("15","30","60");
 
@@ -16,9 +18,11 @@ $room_check_result = $mysql -> query("SELECT * FROM room");
 if($room_check_result){
 	$i = 0;
 	while ($raw = $room_check_result -> fetch_object()){
+		array_push($room_id,htmlspecialchars($raw->id));
 		array_push($room_name, htmlspecialchars($raw->ROOM_NAME));
 		array_push($room_state, htmlspecialchars($raw->ROOM_STATE));
 		array_push($room_type, htmlspecialchars($raw->ROOM_TYPE));
+		array_push($room_member,htmlspecialchars($raw->ROOM_MEMBER));
 	}
 }
 ?>
@@ -36,43 +40,43 @@ if($room_check_result){
 		<h1>Spechen</h1>
 		<p allien = right> ようこそ、<?php echo "$name"; ?>さん</p>
 		<div container>		
-			<form action = "main.php" method = "post">
+			<form action = "main.php" method = "post" >
 				<table>
-					<?php for($i = 0;count($room_name)>$i;$i++){ ?>
-					<?php if($room_state[$i] == -1){ ?>
-					<tr>
-						<td> 
-							<button type="submit" name="example" value="<?php echo $room_name[$i]; ?>" disabled><?php echo $room_name[$i]; ?></button>
-						</td>
-						<td>
-							ディスカッション中です。<?php echo $ROOM_TIME[$room_type[$i]]; ?>分後に終了します。
-						</td>
-					</tr>
-					<?php }else if($room_state[$i] == 0){ ?>
-					<tr>
-						<td>
-							<button type = "submit"	name = "example" value = "<?php echo $room_name[$i]; ?>"><?php echo $room_name[$i]; ?></button>
-						</td>
-						<td>
-							待機中
-						</td>
-					</tr>
-					<?php }else { ?>
-					<tr>
-						<td>
-							<button type = "submit"	name = "example" value = "<?php echo $room_name[$i]; ?>"><?php echo $room_name[$i]; ?></button>				
-						</td>
-						<td>
+					<?php for($i = 0;count($room_id)>$i;$i++){ 
+						$room_state_message = "";
+						$room_disabled ="";
+						if($room_state[$i]){ 
+							$room_state_message = "ディスカッション中です";
+							$room_disabled = "disabled";
+						}else{ 
+							$room_state_message = "
 							待機中<br>				
-							現在の参加人数：<?php echo $room_state[$i]; ?>人
-						</td>
-					</td>
-					<?php  } ?>
-					<?php }?>
-				</table>
-				<input type ="hidden" name = "name" value =<?php echo $name;?>>
-			</form>
-		</div>
-	</div>
-</body>
-</html>
+							現在の参加人数:".$room_member[$i]."人";
+						}?>
+							<tr>
+								<td>
+									<?php echo '<button type = "submit"	name = "'.$room_name[$i].'" value = "'.$room_id[$i].'" onclick = "Onbuttonclick(this);"'.$room_disabled.'>'.$room_name[$i].'</button>'; ?>
+									<input type ="hidden" name ="room_time" value = <?php echo $ROOM_TIME[$room_type[$i]] ?>>		
+								</td>
+								<td>
+									<?php echo $room_state_message; ?>
+								</td>
+							</td>
+
+							<?php }?>
+						</table>
+						<input type ="hidden" name = "name" value =<?php echo $name;?>>
+					</form>
+				</div>
+			</div>
+
+			<?php 
+			echo 	"<script language = 'javascript' type = 'text/javascript'>";
+			echo	"function Onbuttonclicked(button){";
+			$room_add_member = $mysql -> query("UPDATE room SET ROOM_MEMBER = ROOM_MEMBER+1 WHERE id = ". button.value);
+			echo	"}";
+			echo    "</script>"
+			?>
+			<?php $mysql -> close(); ?>
+		</body>
+		</html>
